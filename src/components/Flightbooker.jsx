95eb05ec767794c41fb1e5f72cc202fb350309c4
JOSEPH DON JINS <josephdon.dev@gmail.com>
2025-07-06 21:51:29 +0530

    Update Flightbooker.jsx

--- src/components/Flightbooker.jsx
+++ src/components/Flightbooker.jsx
@@ -3,7 +3,7 @@ import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function Flightbooker() {
-  const[flighttype, setflightType]= useState('one-way');
+  const[flightType, setflightType]= useState('one-way');
   const[departureDate, setDepaturedate]= useState("");
   const[returnDate, setreturnDate]= useState("");
   
